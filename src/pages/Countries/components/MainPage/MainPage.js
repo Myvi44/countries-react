f0902da99762d831343b2aa30135f00098495bb0
Myvi44 <aleksey.arestov.2005@gmail.com>
2023-06-14 20:59:43 +0200

    refactor: modify MainPage.js, create new file: logo.png

--- src/pages/Countries/components/MainPage/MainPage.js
+++ src/pages/Countries/components/MainPage/MainPage.js
@@ -1,7 +1,16 @@
+import image from "./logo.png"
+
+import "./style.css"
+
 export const MainPage = () => {
     return (
-        <>
-            <h1>MainPage</h1>
-        </>
+        <div className="main-page">
+            <div className="main-bg">
+                <h1>MainPage</h1>
+                <span className="text">
+                    Здравствуйте, это сайт про страны.Здесь вы можете увидеть краткое описание более 250 стран, а также посмотреть их на картах Google Maps или openStreetMaps.Надеюсь вам понравиться этот сайт и вы оцените его(это можно сделать на странице «О нас» (ᵔ.ᵔ)
+                </span>
+            </div>
+        </div>
     )
 }
