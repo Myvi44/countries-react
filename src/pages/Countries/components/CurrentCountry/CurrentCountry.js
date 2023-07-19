6dfba598f336aae1f7fa4f04b1f58f768caeb9a7
Myvi44 <aleksey.arestov.2005@gmail.com>
2023-07-19 19:10:27 +0200

    refactor:modify  CurrentCountry.js

--- src/pages/Countries/components/CurrentCountry/CurrentCountry.js
+++ src/pages/Countries/components/CurrentCountry/CurrentCountry.js
@@ -1,7 +1,27 @@
-export const CurrentCountry = ({currentCountry}) => {
+import "./style.css"
+
+export const CurrentCountry = ({ currentCountry }) => {
     return (
-        <>
-        {currentCountry?.name?.official}
-        </>
+        <div className="current-country">
+            <div className="name">
+                {currentCountry?.name?.official}
+            </div>
+            <div className="content">
+                <div className="left-side">
+                    <div className="flags">
+                        <div className="flag">
+                            <img src={currentCountry?.flags?.png} />
+                        </div>
+                        <div className="coat-of-arms">
+                            <img src={currentCountry?.coatOfArms?.png} />
+                        </div>
+                    </div>
+                </div>
+                <div className="right-side">
+
+                </div>
+            </div>
+
+        </div>
     )
 }
